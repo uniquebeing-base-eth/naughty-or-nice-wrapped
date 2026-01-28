0cb33cbd4ae0b15451a6d15010769b61fd40e9be
Uniquebeing <beingunique049@gmail.com>
2026-01-28 19:19:58 +0100

    Update input-otp.tsx

--- src/components/ui/input-otp.tsx
+++ src/components/ui/input-otp.tsx
@@ -8,16 +8,24 @@ const InputOTP = React.forwardRef<React.ElementRef<typeof OTPInput>, React.Compo
   ({ className, containerClassName, ...props }, ref) => (
     <OTPInput
       ref={ref}
-      containerClassName={cn("flex items-center gap-2 has-[:disabled]:opacity-50", containerClassName)}
-      className={cn("disabled:cursor-not-allowed", className)}
+      containerClassName={cn(
+        "flex items-center gap-2 has-[:disabled]:opacity-50",
+        containerClassName
+      )}
+      className={cn(
+        "disabled:cursor-not-allowed focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+        className
+      )}
       {...props}
     />
-  ),
+  )
 );
 InputOTP.displayName = "InputOTP";
 
 const InputOTPGroup = React.forwardRef<React.ElementRef<"div">, React.ComponentPropsWithoutRef<"div">>(
-  ({ className, ...props }, ref) => <div ref={ref} className={cn("flex items-center", className)} {...props} />,
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn("flex items-center", className)} {...props} />
+  )
 );
 InputOTPGroup.displayName = "InputOTPGroup";
 
@@ -34,7 +42,7 @@ const InputOTPSlot = React.forwardRef<
       className={cn(
         "relative flex h-10 w-10 items-center justify-center border-y border-r border-input text-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md",
         isActive && "z-10 ring-2 ring-ring ring-offset-background",
-        className,
+        className
       )}
       {...props}
     >
@@ -51,10 +59,10 @@ InputOTPSlot.displayName = "InputOTPSlot";
 
 const InputOTPSeparator = React.forwardRef<React.ElementRef<"div">, React.ComponentPropsWithoutRef<"div">>(
   ({ ...props }, ref) => (
-    <div ref={ref} role="separator" {...props}>
+    <div ref={ref} role="separator" aria-hidden="true" {...props}>
       <Dot />
     </div>
-  ),
+  )
 );
 InputOTPSeparator.displayName = "InputOTPSeparator";
 
