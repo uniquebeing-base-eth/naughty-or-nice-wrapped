ff3b3d406446fd51d4b76334319957dadc5fcffe
Uniquebeing <beingunique049@gmail.com>
2026-01-28 20:06:25 +0100

    Update index.ts

--- supabase/functions/fetch-users-by-address/index.ts
+++ supabase/functions/fetch-users-by-address/index.ts
@@ -5,11 +5,13 @@ const corsHeaders = {
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 };
 
+
 serve(async (req) => {
   if (req.method === 'OPTIONS') {
     return new Response(null, { headers: corsHeaders });
   }
 
+  
   try {
     const body = await req.json();
     const { addresses, fid } = body;
